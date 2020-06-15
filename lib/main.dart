f0ab3d328c809af8f8577b0a9039024ddacd2602
LarsNyborgPedersen <larsnyborgpedersen@gmail.com>
2020-06-15 12:21:57 +0200

    Popup done and also made stop button

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,15 @@ class MyApp extends StatefulWidget {
   MyAppState createState() => MyAppState();
 }
 
+class Timer {
+  bool isPlaying = false;
+  int minutes = 25;
+}
+
 class MyAppState extends State<MyApp> with TickerProviderStateMixin {
   AnimationController controller;
+  Timer timer = new Timer();
+  TextEditingController customController = new TextEditingController();
 
   // bool isPlaying = false;
 
@@ -33,7 +40,7 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
     super.initState();
     controller = AnimationController(
       vsync: this,
-      duration: Duration(minutes: 25),
+      duration: Duration(minutes: timer.minutes),
     );
 
     // ..addStatusListener((status) {
@@ -86,20 +93,27 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
                             ),
                             GestureDetector(
                               onTap: () {
-                                TextEditingController customController = new TextEditingController();
+                                customController.clear();
+
 
                                 showDialog(context: context, builder: (context) {
                                   return AlertDialog(
                                     title: Text("How many minutes?"),
                                     content: TextField(
-                                      controller: customController
+                                      controller: customController,
+                                      keyboardType: TextInputType.number,
+                                      autofocus:true,
+                                      onSubmitted: (String text) {
+                                        submit();
+                                        
+                                      },
                                     ),
                                     actions: <Widget>[
                                       MaterialButton(
                                         elevation: 5.0,
                                         child: new Text("Submit"),
                                         onPressed: () {
-                                          Navigator.of(context).pop(customController.text.toString());
+                                          submit();
                                         },
                                       )
                                     ],
@@ -132,7 +146,8 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
                     child: AnimatedBuilder(
                       animation: controller,
                       builder: (BuildContext context, Widget child) {
-                        return Icon(controller.isAnimating
+
+                        return Icon(timer.isPlaying
                             ? Icons.pause
                             : Icons.play_arrow);
 
@@ -142,16 +157,34 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
                       },
                     ),
                     onPressed: () {
-                      // setState(() => isPlaying = !isPlaying);
+                      startStopTimer();
+                    },
+                  ),
+                  FloatingActionButton(
+                    child: AnimatedBuilder(
+                      animation: controller,
+                      builder: (BuildContext context, Widget child) {
 
-                      if (controller.isAnimating) {
-                        controller.stop(canceled: true);
-                      } else {
-                        controller.reverse(
-                            from: controller.value == 0.0
-                                ? 1.0
-                                : controller.value);
-                      }
+                        return Icon(Icons.stop);
+
+                        // Icon(isPlaying
+                        // ? Icons.pause
+                        // : Icons.play_arrow);
+                      },
+                    ),
+                    onPressed: () {
+                      timer.isPlaying = false;
+
+
+                      setState(() {
+                        controller.duration = Duration(minutes: timer.minutes);
+                      });
+                      controller.reset();
+                      controller.reverse(
+                          from: controller.value == 0.0
+                              ? 1.0
+                              : controller.value);
+                      controller.stop();
                     },
                   )
                 ],
@@ -161,6 +194,36 @@ class MyAppState extends State<MyApp> with TickerProviderStateMixin {
         ),
       ),
     );
+  }
+  void startStopTimer() {
+    // setState(() => isPlaying = !isPlaying);
+    setState(() {
+      if (timer.isPlaying == true) {
+        timer.isPlaying = false;
+      }
+      else {
+        timer.isPlaying = true;
+      }
+    });
+
+    if (controller.isAnimating) {
+      controller.stop(canceled: true);
+    } else {
+      controller.reverse(
+          from: controller.value == 0.0
+              ? 1.0
+              : controller.value);
+    }
+  }
+
+  void submit() {
+    Navigator.of(context).pop(customController.text.toString());
+    timer.minutes = int.parse(customController.text);
+    controller.duration = Duration(minutes: timer.minutes);
+
+    controller.reset();
+    startStopTimer();
+    timer.isPlaying = true;
   }
 }
 
